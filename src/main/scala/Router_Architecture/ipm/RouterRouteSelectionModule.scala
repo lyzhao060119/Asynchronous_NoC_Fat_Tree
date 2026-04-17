2fe41682b3cc9199ffedcb35cd9709115089abd4
lyzhao060119 <1765940063@qq.com>
2026-04-17 15:16:26 +0800

    一些无关紧要的格式更新

--- src/main/scala/Router_Architecture/ipm/RouterRouteSelectionModule.scala
+++ src/main/scala/Router_Architecture/ipm/RouterRouteSelectionModule.scala
@@ -5,13 +5,12 @@ import Router_Architecture.common.RouterModuleConfig
 import chisel3._
 import chisel3.util.log2Ceil
 
-/**
- * Computes route destinations for head flits and reuses the stored
- * direction vector for body/tail flits.
- */
+/** Computes route destinations for head flits and reuses the stored direction
+  * vector for body/tail flits.
+  */
 class RouterRouteSelectionModule(
-  config: RouterModuleConfig,
-  computeHeadRouting: (Packet, Bool, UInt) => Vec[Bool]
+    config: RouterModuleConfig,
+    computeHeadRouting: (Packet, Bool, UInt) => Vec[Bool]
 ) extends Module {
   private val dirW = math.max(1, log2Ceil(config.nDirs))
   val io = IO(new Bundle {
@@ -19,13 +18,15 @@ class RouterRouteSelectionModule(
     val inValid = Input(Vec(config.totalPorts, Bool()))
     val isHead = Input(Vec(config.totalPorts, Bool()))
     val storedDir = Input(Vec(config.totalPorts, Vec(config.nDirs, Bool())))
-    val currentDestVec = Output(Vec(config.totalPorts, Vec(config.nDirs, Bool())))
+    val currentDestVec =
+      Output(Vec(config.totalPorts, Vec(config.nDirs, Bool())))
   })
 
   for (i <- 0 until config.totalPorts) {
     val ingressDir = config.dirOfPhys(i).U(dirW.W)
     // The ingress direction is implied by the physical port index.
-    val headDecision = computeHeadRouting(io.inBits(i), io.inValid(i), ingressDir)
+    val headDecision =
+      computeHeadRouting(io.inBits(i), io.inValid(i), ingressDir)
     when(io.isHead(i)) {
       io.currentDestVec(i) := headDecision
     }.otherwise {
